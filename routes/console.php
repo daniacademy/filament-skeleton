841e5f283202f3d0cfb3887ca0d53f5d099c4010
Daniel Alejandro Fernandez <dfernandez@unpaz.edu.ar>
2024-12-07 08:54:49 -0300

    fix: change Queue health checking

--- routes/console.php
+++ routes/console.php
@@ -1,10 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Schedule;
+use Spatie\Health\Commands\DispatchQueueCheckJobsCommand;
 
+Schedule::command(DispatchQueueCheckJobsCommand::class)->everyMinute();
 Schedule::command('queue:work --stop-when-empty')->everyMinute()
     ->withoutOverlapping();
-Schedule::command('health:queue-check-heartbeat')->everyMinute();
 Schedule::command('logs:clean')->daily()->at('00:00');
 Schedule::command('audit:clean')->daily()->at('00:30');
 Schedule::command('backup:clean')->daily()->at('01:00');
