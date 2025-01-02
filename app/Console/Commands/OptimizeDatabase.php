006ffbef212849736f9f453653186854a4da2b62
Daniel Alejandro Fernandez <dfernandez@unpaz.edu.ar>
2025-01-02 17:54:10 -0300

    refactor: Your application does not contain env function calls outside of your config files

--- app/Console/Commands/OptimizeDatabase.php
+++ app/Console/Commands/OptimizeDatabase.php
@@ -28,10 +28,13 @@ class OptimizeDatabase extends Command
     {
         $this->info('Starting database optimization...');
 
+        $defaultConnection = config('database.default');
+        $databaseName = config("database.connections.{$defaultConnection}.database");
+
         $tables = DB::select('SHOW TABLES');
 
         foreach ($tables as $table) {
-            $tableName = $table->{'Tables_in_' . env('DB_DATABASE')};
+            $tableName = $table->{'Tables_in_' . $databaseName};
             DB::statement("OPTIMIZE TABLE {$tableName}");
             $this->info("Optimized table: {$tableName}");
         }
