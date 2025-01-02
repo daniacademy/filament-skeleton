81af1546e7a99d246d1739328080828dc1d85ee8
Daniel Alejandro Fernandez <dfernandez@unpaz.edu.ar>
2025-01-02 18:04:14 -0300

    refactor: Your application defines custom error page views

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+
+<head>
+    <meta charset="UTF-8">
+    <title>IGLOO</title>
+    <meta name="description" content="IGLOO">
+    <meta name="keywords" content="IGLOO, IGLOO, IGLOO, IGLOO, IGLOO, IGLOO, IGLOO, IGLOO, IGLOO, IGLOO">
+    <meta name="author" content="Sac Solutions">
+    <meta name="ROBOTS" content="INDEX, ALL, FOLLOW">
+    <meta name="revisit-after" content="1 day">
+    <meta name="RATING" content="General">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Estilos -->
+    <link rel="stylesheet" href="{{ asset('503/css/containers.css') }}">
+    <link rel="stylesheet" href="{{ asset('503/css/main.css') }}">
+    <script src="https://kit.fontawesome.com/9adff69123.js" crossorigin="anonymous"></script>
+
+    <!-- Librería JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Favicon Simple -->
+    <link rel="icon" type="image/png" href="{{ asset('503/img/favicon.png') }}" />
+
+    <!-- Tipografías  -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext"
+        rel="stylesheet">
+
+</head>
+
+<body class="parallax">
+    <div class="parallax-text">
+        <div class="logo"><img src="{{ asset('503/img/logo.png') }}" alt="" class="img-responsive"></div>
+        <div class="datos">
+            <h1>ESTAMOS DESARROLLANDO <br>EL NUEVO SITIO WEB</h1>
+
+        </div>
+    </div>
+</body>
+
+</html>
